658e71fc332a0870a343e7a9426bf0568248a9b9
novvvi <novehui@gmail.com>
2019-05-26 19:33:11 -0500

    master

---
+++ autosetup.py
@@ -0,0 +1,76 @@
+import os
+import shutil
+import subprocess
+import time
+
+
+
+class django(): 
+    def __init__(self, call, project, app):
+        self.local_dir = os.path.dirname(os.path.realpath(__file__)) 
+        print (self.local_dir)
+        self.call = call
+        self.project = project
+        self.app = app
+        self.final = self.final(self.project)
+        #.create_app = self.create_app(self.project, self.call, self.app)
+        #self.copyurl = self.copyurl(self.local_dir, self.project, self.app)
+        #self.settings_update = self.settings_update(self.app, self.project)
+        #self.urls_update = self.urls_update(self.app, self.project)
+
+
+
+    def create_app(self, project, python, apps):
+        for app in apps:
+            text = f'cd {project}/apps & {python} ../manage.py startapp {app} & cd {app}/ & mkdir templates & mkdir static & cd templates/ & mkdir {app}'
+            test = os.system(text)
+            print (test)
+
+
+    def copyurl(self, direct, project, apps):
+        for app in apps: 
+            src = f"{direct}/tmp/urls.py"
+            des= f"{direct}/{project}/apps/{app}/urls.py"
+            shutil.copyfile(src, des)
+
+
+    def final(self, project):
+        comm1 = f'django-admin startproject {project}'
+        comm2 = f'mkdir {project}/apps'
+        run1 = os.system(comm1)
+        print (run1)
+        print ("wait 3 secs until startproject complated =D")
+        time.sleep(3)
+        run2 = os.system(comm2)
+        print (run2)
+
+
+    def settings_update(self, apps, project):
+        setting = "INSTALLED_APPS = ["
+        for app in apps:
+            setting += (f"\n        'apps.{app}','")
+        s = open(f"{project}/{project}/settings.py").read()
+        s = s.replace("INSTALLED_APPS = [", setting)
+        f = open(f"{project}/{project}/settings.py", 'w')
+        f.write(s)
+        f.close()
+
+
+    def urls_update(self, apps, project):
+        url = "    url(r'^admin/', admin.site.urls),"
+        for app in apps:
+            url += (f"\n    #url(r'^', include('apps.{app}.urls'),")
+        s = open(f"{project}/{project}/urls.py").read()
+        s = s.replace("    url(r'^admin/', admin.site.urls),", url)
+        s = s.replace("from django.conf.urls import url", "from django.conf.urls import url, include")
+        f = open(f"{project}/{project}/urls.py", 'w')
+        f.write(s)
+        f.close()
+
+
+if __name__ == "__main__":
+    py = input("Call py in cmd :")
+    p_name = input("your project name :")
+    app_names = input("your apps name Ex.(app1,app2) :").replace(" ", "").split(",")
+    print (app_names)
+    django( py, p_name,  app_names)
---
+++ tmp/urls.py
@@ -0,0 +1,14 @@
+# (?P<num>\d+)  accept number
+# (?P<name>\w+)  accept char
+
+from django.conf.urls import url
+from . import views
+
+
+urlpatterns = [
+    #url(r'^$', views.function),
+    #url(r'^$', views.function),
+    #url(r'^$', views.function),
+    #url(r'^$', views.function),
+    #url(r'^$', views.function),
+]
